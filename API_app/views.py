f25ed2b103e07104ef4ba06286bbf52f4b127d79
Prabhneet <prabhneet13@gmail.com>
2023-05-04 14:08:31 +0530

    GET request

--- API_app/views.py
+++ API_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from .models import Student
+from .serializers import StudentSerializer
+from django.http import HttpResponse
 # Create your views here.
+
+
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythonData = JSONParser.parse(stream)
+        id = pythonData.get('id', None)
+        if id is not None:
+            student_data = Student.objects.get(id=id)
+            serializer = StudentSerializer(data=student_data)
+            json_data = JSONRenderer.render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+        else:
+            student_data = Student.objects.all()
+            serializer = StudentSerializer(student_data, many=True)
+            json_data = JSONRenderer.render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
